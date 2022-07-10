15d8f0875a60090519340422c7f1d39ee2375a13
Imanuddin.Ardian <imanuddin.ardian@gmail.com>
2022-07-10 10:43:46 +0700

    changeOrigin false on proxy

--- config/proxy.js
+++ config/proxy.js
@@ -3,5 +3,6 @@ export default {
         target: 'https://backend-autofintion.microlance.id/',
         // pathRewrite: { '^/api/': '' },
         // changeOrigin: true
+        changeOrigin: false,
     }
 }
