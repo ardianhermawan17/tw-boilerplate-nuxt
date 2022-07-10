071c6cf2fb5f787883ac388c3196c98902a673f3
Imanuddin.Ardian <imanuddin.ardian@gmail.com>
2022-07-10 14:56:03 +0700

    add base url

--- config/axios.js
+++ config/axios.js
@@ -1,6 +1,6 @@
 export default {
     //  When using proxy dont user baseURL
-    // baseURL: 'https://backend-autofintion.microlance.id/',
+    baseURL: 'https://backend-autofintion.microlance.id/',
     // common: {
     //     'Accept': 'application/json, text/plain, */*'
     // },
