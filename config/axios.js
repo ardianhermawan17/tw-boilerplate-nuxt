a4dc5dd2e727f682695593e1926d3c26dc62e9b7
Imanuddin.Ardian <imanuddin.ardian@gmail.com>
2022-07-11 12:08:54 +0700

    with origin true

--- config/axios.js
+++ config/axios.js
@@ -1,8 +1,8 @@
 export default {
     //  When using proxy dont user baseURL
-    baseURL: 'https://backend-autofintion.microlance.id/',
+    // baseURL: 'https://backend-autofintion.microlance.id/',
     // common: {
     //     'Accept': 'application/json, text/plain, */*'
     // },
-    // proxy: true
+    proxy: true
 }
