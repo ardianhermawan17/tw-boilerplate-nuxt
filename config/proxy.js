d9420ea9cb5f8f988c0d127f675df87eebea5211
Imanuddin.Ardian <imanuddin.ardian@gmail.com>
2022-07-11 12:19:46 +0700

    public proxy

--- config/proxy.js
+++ config/proxy.js
@@ -1,15 +1,15 @@
 export default {
-    // '/api/': {
-    //     target: 'https://backend-autofintion.microlance.id/',
-    //     // pathRewrite: { "^/api/": "" },
-    //     // ws: false
-    //     changeOrigin: true
-    // },
-    'https://autofintion.vercel.app/api/': {
+    '/api/': {
         target: 'https://backend-autofintion.microlance.id/',
-        pathRewrite: { "https://autofintion.vercel.app/api/": "https://backend-autofintion.microlance.id/api/" },
+        pathRewrite: { "^/api/": "/api/public/" },
         // ws: false
         changeOrigin: true
-    }
+    },
+    // 'https://autofintion.vercel.app/api/': {
+    //     target: 'https://backend-autofintion.microlance.id/',
+    //     pathRewrite: { "https://autofintion.vercel.app/api/": "https://backend-autofintion.microlance.id/api/" },
+    //     // ws: false
+    //     changeOrigin: true
+    // }
     // '/api/': 'https://backend-autofintion.microlance.id/'
 }
